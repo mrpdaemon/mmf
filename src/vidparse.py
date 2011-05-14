efe22868a832f235be09ec5b98fa7ca36b557961
Mark Pariente <markpariente@gmail.com>
2011-05-14 01:14:59 -0700

    Increase parsing precision by using string.startswith() for field headers.

--- src/vidparse.py
+++ src/vidparse.py
@@ -57,39 +57,37 @@ class VidParser:
             elif (mp_line == "Text"):
                 current_section = TEXT_SECTION
             elif current_section == VIDEO_SECTION:
-                if  "Format  " in mp_line:
+                if mp_line.startswith("Format  "):
                     self.vid_format = get_field_value(mp_line)
-                elif  "Codec ID " in mp_line:
+                elif mp_line.startswith("Codec ID "):
                     self.vid_codec_id = get_field_value(mp_line)
-                elif  "ID  " in mp_line:
-                    # Codec ID check needs to be above stream ID check
-                    self.vid_stream_id = int(get_field_value(mp_line))
-                elif "Scan type " in mp_line:
+                elif  mp_line.startswith("ID "):
+                    self.vid_stream_id = int(tokenize_field(get_field_value(mp_line))[0])
+                elif mp_line.startswith("Scan type "):
                     self.vid_scan = get_field_value(mp_line)
-                elif "Width " in mp_line:
+                elif mp_line.startswith("Width "):
                     vid_width_str = get_field_value(mp_line)
                     self.vid_width = int(field_collapse_thousands(vid_width_str)[0])
-                elif "Height " in mp_line:
+                elif mp_line.startswith("Height "):
                     vid_height_str = get_field_value(mp_line)
                     self.vid_height = int(field_collapse_thousands(vid_height_str)[0])
-                elif "Bit rate  " in mp_line:
+                elif mp_line.startswith("Bit rate  "):
                     vid_bit_rate_str = get_field_value(mp_line)
                     self.vid_bitrate = int(bit_rate_convert(field_collapse_thousands(vid_bit_rate_str)))
             elif current_section == AUDIO_SECTION:
-                if  "Format  " in mp_line:
+                if mp_line.startswith("Format  "):
                     self.audio_format = get_field_value(mp_line)
-                elif  "Codec ID " in mp_line:
+                elif mp_line.startswith("Codec ID "):
                     self.audio_codec_id = get_field_value(mp_line)
-                elif  "ID  " in mp_line:
-                    # Codec ID check needs to be above stream ID check
-                    self.audio_stream_id = int(get_field_value(mp_line))
-                elif "Bit rate  " in mp_line:
+                elif mp_line.startswith("ID"):
+                    self.audio_stream_id = int(tokenize_field(get_field_value(mp_line))[0])
+                elif mp_line.startswith("Bit rate  "):
                     audio_bit_rate_str = get_field_value(mp_line)
                     self.audio_bitrate = int(bit_rate_convert(field_collapse_thousands(audio_bit_rate_str)))
-                elif "Sampling rate " in mp_line:
+                elif mp_line.startswith("Sampling rate "):
                     audio_sample_rate_str = get_field_value(mp_line)
                     self.audio_samplerate = int(float_to_str_trunc(float(tokenize_field(audio_sample_rate_str)[0])))
-                elif "Channel(s) " in mp_line:
+                elif mp_line.startswith("Channel(s) "):
                     audio_channel_str = get_field_value(mp_line)
                     self.audio_channels = int(tokenize_field(audio_channel_str)[0])
         
